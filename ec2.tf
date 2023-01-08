c3d55ed578b523a9b4ffd8f50490dde3c1c51834
pramod-devops <107288148+pramod-devops@users.noreply.github.com>
2023-01-08 15:28:22 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-  ami           = "ami-0c6a6b0e75b2b6ce7"
+  ami           = "ami-01216e7612243e0ef" 
   count         = "${var.ec2_count}"
   instance_type = "t2.micro"
 
